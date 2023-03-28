532583da5b855aa4a20de54db63040970b1d5bb6
LolkaOpka <roman15032002@mail.ru>
2023-03-28 21:35:44 +0300

    Добавлен файл с классом модуля "Монтажники"

---
+++ Program/Project/Workers/Workers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Workers
+    {
+        int id_worker;
+        string name;
+        int id_zayavki;
+
+
+        Workers() { }
+
+    }
+}
