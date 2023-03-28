ac915d5cd3cc7c8b3be437fe51d1a4f466c362de
LolkaOpka <roman15032002@mail.ru>
2023-03-28 21:57:37 +0300

    Добавлен файл класса Abonst.cs

---
+++ Program/Project/Abonst/Abonst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Abonst
+    {
+        int id_abonworker;
+        string name;
+        int id_zayavki;
+
+        Abonst() { }
+    }
+}
