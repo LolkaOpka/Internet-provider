7ddb8e1fb2ee4b7df1298921c95b05be0fd055da
LolkaOpka <roman15032002@mail.ru>
2023-03-28 23:49:36 +0300

    Создание Workers.cs для конфликта слияния

---
+++ Program/Project/Workers/Workers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Workers
+    {
+        int id_worker=0;
+        string name;
+        int id_zayavki;
+
+        void ChangeZayavka() { }
+        void AddClient() { }
+        void UpdateClient() { }
+        void DeleteClient() { }
+        void UpdateZayavka() { }
+        void CreateOtchet() { }
+        Workers() { }
+
+    }
+}
